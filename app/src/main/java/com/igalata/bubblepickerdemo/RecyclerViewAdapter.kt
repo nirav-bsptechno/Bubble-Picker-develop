afa11c8805c19c3ded504320b27f1fb8c88740fc
BSP Techno <nirav@bsptechno.com>
2018-09-25 15:52:27 +0530

    First Commit.
    First Commit - selecting location selection will display list of images.

---
+++ app/src/main/java/com/igalata/bubblepickerdemo/RecyclerViewAdapter.kt
@@ -0,0 +1,34 @@
+package com.igalata.bubblepickerdemo
+
+import android.graphics.drawable.Drawable
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+
+class RecyclerViewAdapter(val list:ArrayList<Drawable?>): RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewAdapter.ViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.raw_image_list, parent, false)
+        return ViewHolder(v)
+    }
+
+    //this method is binding the data on the list
+    override fun onBindViewHolder(holder: RecyclerViewAdapter.ViewHolder, position: Int) {
+        holder.bindItems(list[position])
+    }
+
+    //this method is giving the size of the list
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        fun bindItems(data : Drawable?){
+            val _imageView: ImageView = itemView.findViewById(R.id.image)
+            _imageView.setImageDrawable(data)
+        }
+
+    }
+}
